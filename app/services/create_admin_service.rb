4269ead7a54cba1d5c44b44566da925a1df0b18a
kli <A71@kli.wireless.yale.internal>
2014-04-30 07:44:13 -0400

    Fixed db seed

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,9 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: => "kevin.z.li@yale.edu") do |user|
-        user.password = "changeme"
-        user.password_confirmation = "changeme"
-        user.confirm!
-        user.admin!
-      end
+    	user = User.find_or_create_by!(email: => "kevin.z.li@yale.edu") 
+	        user.password = "changeme"
+	        user.password_confirmation = "changeme"
+	        user.confirm!
+	        user.admin!
   end
 end
