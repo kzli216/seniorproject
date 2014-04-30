f95e7d5ef25fad465298f20d037f6e1c5907388f
kli <A71@kli.wireless.yale.internal>
2014-04-30 08:31:35 -0400

    Fixed active mailer smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,14 +73,15 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.mailgun.org",
     port: 587,
     domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SANDBOX_USERNAME"],
+    password: ENV["SANDBOX_PASSWORD"]
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
   config.action_mailer.delivery_method = :smtp
